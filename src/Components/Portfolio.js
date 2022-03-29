a4b82b8a66e9f8797be2da3fd0f8957c45d482ed
abhijit <abhi.pachpande@gmail.com>
2022-03-29 13:16:08 +0530

    initial commit

---
+++ src/Components/Portfolio.js
@@ -0,0 +1,47 @@
+import React, { Component } from "react";
+import Zmage from "react-zmage";
+import Fade from "react-reveal";
+
+let id = 0;
+class Portfolio extends Component {
+  render() {
+    if (!this.props.data) return null;
+
+    const projects = this.props.data.projects.map(function (projects) {
+      let projectImage = "images/portfolio/" + projects.image;
+
+      return (
+        <div key={id++} className="columns portfolio-item align-items-center">
+          <div className="item-wrap">
+            
+            <Zmage alt={projects.title} src={projectImage} />
+            <a href={projects.url} class="work-anchor">
+            <div style={{ textAlign: "center" }}>{projects.title}</div>
+            </a>
+          </div>
+        </div>
+      );
+    });
+
+    return (
+      <section id="portfolio">
+        <Fade left duration={1000} distance="40px">
+          <div className="row">
+            <div className="twelve columns collapsed">
+              <h1>Check Out Some of My Works.</h1>
+
+              <div
+                id="portfolio-wrapper"
+                className="bgrid-quarters s-bgrid-thirds cf"
+              >
+                {projects}
+              </div>
+            </div>
+          </div>
+        </Fade>
+      </section>
+    );
+  }
+}
+
+export default Portfolio;
